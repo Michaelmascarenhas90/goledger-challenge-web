4007fc54aea9f0956aab9c8fda28ba86e46ece00
ultratalkchat <ultratalk.contato@gmail.com>
2024-12-08 21:35:07 -0300

    chore: adjustment style component Header

--- goledger-challenge-front/src/components/Header/styles.ts
+++ goledger-challenge-front/src/components/Header/styles.ts
@@ -3,9 +3,12 @@ import theme from "../../theme";
 
 export const Header = styled.header`
   width: 100vw;
-  display: flex;
-  padding-left: ${theme.padding.medium};
-  padding-right: ${theme.padding.medium};
   position: fixed;
   top: 0;
+
+  display: flex;
+  height: 40px;
+  align-items: center;
+  padding-left: ${theme.padding.medium};
+  padding-right: ${theme.padding.medium};
 `
